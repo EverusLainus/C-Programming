3d1901ebfbd1679bc39e3e3a2023bf8b78baff55
Everus M <everus@Everuss-MacBook-Air.local>
2022-03-07 21:16:31 +0530

    add a file

---
+++ codeacademy/PassFail.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a=45;
+    b=25.25;
+    if (a > 50)
+    {
+        printf("passed the test");
+    }
+    if (b< 50)
+    {
+    printf("failed the test");
+    }
+    return 0;
+} 
