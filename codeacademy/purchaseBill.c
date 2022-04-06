ca97e85dac398282aeae02de4d5b16b5d1ced4f8
Everus <everuslainus@gmail.com>
2022-04-06 22:54:28 +0530

    purchase bill

---
+++ codeacademy/purchaseBill.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("number of shirt of price 120 rupees:");
+    scanf("%d", &a);
+    printf("total amount is : %d", a*120);
+    return 0;
+}
