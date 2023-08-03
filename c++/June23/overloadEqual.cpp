6a11fe17b22cb255fa1848ddca85c837b8378cff
Everus <everuslainus@gmail.com>
2023-08-03 16:54:47 +0530

    overload operator ==

---
+++ c++/June23/overloadEqual.cpp
@@ -0,0 +1,34 @@
+//overload == operator for vectors
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class vector_class{
+    public:
+    int vector1;
+    int vector2;
+    vector_class(int a=0, int b=0 ){
+        vector1=a;
+        vector2=b;
+    }
+
+    //can be overloaded globally
+    bool operator== (vector_class v){
+
+            //== on each vector index
+            if((this->vector1 ==v.vector1) && (this->vector2 ==v.vector2)) {
+                return true;
+
+            }
+        return false;
+    }
+
+};
+
+int main(){
+    vector_class v1(15,7);
+    vector_class v2(15,7);
+    bool result = (v1==v2);
+    cout << result;
+    return 0;
+}
