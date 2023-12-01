9b6cec90e7232fd0732d4a38915c5a267bfcbd0b
Everus <everuslainus@gmail.com>
2023-12-01 17:43:02 +0530

    swap

---
+++ c++/June23/swap.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+string str1 = "horizon";
+string str2="gi";
+str2.swap(str1);
+cout <<"swap "<<str1 <<" with "<<str2 <<endl;
+return 0;
+}
