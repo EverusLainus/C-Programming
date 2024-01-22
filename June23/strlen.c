938f87e19201a57aba34d0d4a199fe3de6beb0f3
Everus <everuslainus@gmail.com>
2024-01-22 22:44:36 +0530

    strlen

--- June23/strlen.c
+++ June23/strlen.c
@@ -4,11 +4,14 @@
 #define MAX 240
 
 //strlen give one more than the actual length
+//requires a null terminated string a argument. 
+//it wil count till it reaches null character
+//The behavior of strlen with a memory block that isn't explicitly null-terminated is undefined.
 
 int main(){
     int len=0;
     char input[MAX];
-    fgets(input, MAX , stdin);
+    fgets( input, MAX , stdin);
     len = strlen(input);
     printf("length of of input is %d \n", len);
     return 0;
